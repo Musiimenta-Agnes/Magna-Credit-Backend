095d96e48d6861fd77efd1a24b40fc08f3d15031
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2026-02-18 12:32:28 +0300

    second  loan application

---
+++ app/Models/LoanApplicationDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoanApplicationDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'loan_application_id',
+        'kin_name',
+        'kin_contact',
+        'occupation',
+        'monthly_income',
+        'loan_type',
+        'education',
+        'address',
+        'national_id_image',
+        'collateral_images',
+    ];
+
+    protected $casts = [
+        'collateral_images' => 'array',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LoanApplicationController;
+use App\Http\Controllers\Api\LoanApplicationDetailController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/loan-applications', [LoanApplicationController::class, 'store']);
+Route::post('/loan-application-details', [LoanApplicationDetailController::class, 'store']);
