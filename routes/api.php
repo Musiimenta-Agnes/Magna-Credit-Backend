8e89339abbb0a5d87cc4c0165b2b067aafe080d9
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2026-02-22 23:13:53 +0300

    forgot password

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LoanApplicationController;
 use App\Http\Controllers\Api\LoanApplicationDetailController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/loan-applications', [LoanApplicationController::class, 'store']);
 Route::post('/loan-application-details', [LoanApplicationDetailController::class, 'store']);
+
+
+Route::post('/forgot-password/send-code', [ForgotPasswordController::class, 'sendCode']);
+Route::post('/forgot-password/verify-code', [ForgotPasswordController::class, 'verifyCode']);
+Route::post('/forgot-password/reset-password', [ForgotPasswordController::class, 'resetPassword']);
